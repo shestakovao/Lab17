dd44e4508fc9e46f9819f1066d6916dd55c5d514
Алексей Шестаков <shestakov_ao@bk.ru>
2022-11-05 14:18:36 +0700

    Добавьте файлы проекта.

---
+++ Task1/BankAccounts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class BankAccounts<T>
+    {
+        private T AccountNumber { set; get; }
+        private double Balance { set; get; }
+        private string FullName { set; get; }
+
+        public void Input() 
+        {
+            Console.WriteLine("Введите номер счёта");
+            AccountNumber = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+
+            Console.WriteLine("Введите ФИО владельца");
+            FullName = Console.ReadLine();
+
+            Console.WriteLine("Введите баланс счёта");
+            Balance = Convert.ToDouble(Console.ReadLine()); 
+        }
+
+        public string GetInfo()
+        {
+            return $"{AccountNumber} {FullName} {Balance}";        
+        }
+
+
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccounts<int> bankAccounts1 = new BankAccounts<int>();
+            bankAccounts1.Input();
+            Console.WriteLine(bankAccounts1.GetInfo());
+
+            BankAccounts<string> bankAccounts2 = new BankAccounts<string>();
+            bankAccounts2.Input();
+            Console.WriteLine(bankAccounts2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
